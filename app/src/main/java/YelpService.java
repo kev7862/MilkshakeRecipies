8ceb4aef6bef55797ae869325b742f17e4ede2fe
kev7862 <kevmatite7862@gmail.com>
2017-09-25 17:34:39 +0300

    Creating another request using the new URL

--- app/src/main/java/YelpService.java
+++ app/src/main/java/YelpService.java
@@ -1,6 +1,7 @@
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 /**
  * Created by matite on 9/25/17.
@@ -22,5 +23,8 @@ public class YelpService {
         HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.YELP_BASE_URL).newBuilder();
         urlBuilder.addQueryParameter(Constants.YELP_LOCATION_QUERY_PARAMETER, location);
         String url = urlBuilder.build().toString();
+        Request request= new Request.Builder()
+                .url(url)
+                .build();
     }
 }
